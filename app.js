6f8873871f42753d362f575fb125ba1e54450212
anshyadav <anshyadav4244@gmail.com>
2022-04-14 23:45:39 +0530

    products with their id

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: "Working fine",
-  });
-});
+const productRoute = require("./api/route/products");
+
+app.use("/products", productRoute);
 
 module.exports = app;
